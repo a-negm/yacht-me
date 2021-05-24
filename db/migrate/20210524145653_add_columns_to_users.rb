f92d1b87f1d1c1737e517a600d8bc110de26544e
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-24 16:12:27 +0100

    Implemented further changes to the user sign up process

---
+++ db/migrate/20210524145653_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_column :users, :address, :string
+    add_column :users, :date_of_birth, :string
+  end
+end
