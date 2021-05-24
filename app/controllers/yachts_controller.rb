e66ad6f8fb113ec7a8889db108b9159ccb4c2581
ahmed negm <ahmednegm86@gmail.com>
2021-05-24 16:36:17 +0200

    added yachts controller

---
+++ app/controllers/yachts_controller.rb
@@ -0,0 +1,2 @@
+class YachtsController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_140233) do
+ActiveRecord::Schema.define(version: 2021_05_24_142100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,19 @@ ActiveRecord::Schema.define(version: 2021_05_24_140233) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "yachts", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.integer "length"
+    t.integer "number_of_cabins"
+    t.integer "rental_price"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.integer "guests"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_yachts_on_user_id"
+  end
+
+  add_foreign_key "yachts", "users"
 end
