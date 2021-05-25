4a99cab622fb12df30f05cf30c0f610e8e86de81
Jan Peter <masteryourownway@gmail.com>
2021-05-25 12:30:53 +0200

    indexpage setup

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -1,5 +1,10 @@
 class YachtsController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: :index
+  
+  def index
+    
+  end
+  
   def show
     @yacht = Yacht.find(params[:id])
   end
