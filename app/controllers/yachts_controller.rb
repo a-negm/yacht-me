955d82aa6bbeeb952fe18d552588609538b4c303
ahmed negm <ahmednegm86@gmail.com>
2021-05-25 15:01:02 +0200

    updating show page

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -1,10 +1,18 @@
 class YachtsController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @yachts = Yacht.all
   end
 
   def show
     @yacht = Yacht.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
   end
 end
