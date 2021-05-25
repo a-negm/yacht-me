3de88516fde40b35dfc7b46b2d648064940f3d69
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-25 14:59:17 +0100

    Added styling to the booking form

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :yachts do
-    get '/booking_successful', to: "bookings#successful", as: :booking_successful
     resources :bookings, only: %i[new create]
+
+     get '/booking_successful', to: "bookings#successful", as: :booking_successful
   end
+     get '/my-bookings', to: 'bookings#index', as: :my_bookings
+
 
 end
