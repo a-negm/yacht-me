266dcc4db6a17e3a3117c7c5b4bdf052ccf74a2b
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-25 11:32:19 +0100

    Made changes to the booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :new
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = params[:user_id]
+    @booking.yacht = params[:yacht_id]
+    if @booking.save
+      redirect_to booking_successful_path
+    else
+      render :new
+    end
+  end
+
+
+  def booking_params
+    params.require(:booking).permit(:check_out_date, :check_in_date)
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :yacht
   belongs_to :user
+
+  validates_presence_of :user_id, :yacht_id, :check_out_date, :check_in_date
 end
