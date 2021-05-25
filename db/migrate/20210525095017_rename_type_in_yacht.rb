28e3cdceceb1f780f72e183de50f5c2009a55be0
ahmed negm <ahmednegm86@gmail.com>
2021-05-25 16:32:07 +0200

    added show page

--- db/migrate/20210525095017_rename_type_in_yacht.rb
+++
@@ -1,5 +0,0 @@
-class RenameTypeInYacht < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :yachts, :type, :yacht_type
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(name)
+
+bluewhale = Yacht.new(
+  name: 'blue whale',
+  length: 33,
+  description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo do"
+)
+
+bluewhale.save!
+
+puts "#{bluewhale} has been created"
