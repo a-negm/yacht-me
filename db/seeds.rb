bf4fc9ca4b13bd1744c38a0efd08265980a7ac33
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-26 13:11:50 +0100

    Added Url column to yachts table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+# Yacht.new(name: "HMS Eccles",
+#      yacht_type: "sail-yacht",
+#      length: 60,
+#      number_of_cabins: 4,
+#      rental_price: 200,
+#      description: "Beautiful yacht based in the French Riveria. Best used in the summer.",
+#      user_id: 1,
+#      guests: 2)
+
+
+
+# Yacht.new(name: "The Marquess",
+#      yacht_type: "sail-yacht",
+#      length: 50,
+#      number_of_cabins: 4,
+#      rental_price: 400,
+#      description: "A classic boat anchored at Southampton. A vintage .",
+#      user_id: 1,
+#      guests: 2)
