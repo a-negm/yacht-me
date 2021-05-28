2acc3da7491eebbb21f371174f3083cb3ef4b6a4
ahmed negm <ahmednegm86@gmail.com>
2021-05-28 15:08:46 +0200

    conflicting migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_05_28_100434) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +113,7 @@ ActiveRecord::Schema.define(version: 2021_05_28_100434) do
     t.integer "guests"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
     t.string "location"
     t.float "longitude"
     t.float "latitude"
