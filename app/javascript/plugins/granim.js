989b832646fd9e3bd06c63f08a6a26857ceb4e36
ahmed negm <ahmednegm86@gmail.com>
2021-05-28 10:33:10 +0200

    style updates

--- app/javascript/plugins/granim.js
+++ app/javascript/plugins/granim.js
@@ -7,7 +7,7 @@ const granimInstance = new Granim({
   states : {
       "default-state": {
           gradients: [
-              ['#AA076B', '#61045F'],
+
               ['#02AAB0', '#00CDAC'],
               ['#DA22FF', '#9733EE']
           ]
